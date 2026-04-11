a688ad11d601e64408b1de7b246e1220531d5bb8
Mohammed Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2026-04-11 22:28:29 +0600

    update

--- 5th Semister/Microprocessor lab/mid/Mahbub Sir/print_string.asm
+++
@@ -1,7 +0,0 @@
-org 100h
-
-    mov dx, offset msg
-    mov ah, 09h
-    int 21h
-
-msg db 'Hello, World!$'
--- 5th Semister/app development/family health/android/app/src/main/kotlin/com/example/family_health_organization/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.family_health_organization
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
